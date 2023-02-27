c753c7d2260117803b1c851052e51a773bb28c85
Muhammad Naufal Arkan <100178289+Flwstrm40@users.noreply.github.com>
2023-02-27 09:51:19 +0700

    Update MTitik.java

--- Praktikum 1/MTitik.java
+++ Praktikum 1/MTitik.java
@@ -16,10 +16,8 @@ class MTitik {
         t2.setOrdinat(4.0);
         
         Titik t3 = new Titik(5, 6);
-		
-        int counterTitik = t1.getCounterTitik();
         
-        System.out.println("Jumlah objek titik : " + counterTitik);
+        System.out.println("Jumlah objek titik : " + Titik.counterTitik);
         System.out.println("t1(" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
         System.out.println("t2(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
         System.out.println("t3(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
